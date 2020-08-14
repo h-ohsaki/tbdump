23a514c9fb7df2d736943dac80f6ec54a1093a90
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2020-08-14 22:30:41 +0900

    require ansiterm

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="perlcompat",
+    name="tbdump",
     version="1.1",
     author="Hiroyuki Ohsaki",
     author_email="ohsaki@lsnl.jp",
-    description="Perl-like utility functions such as warn, die, getopt, and require",
+    description=
+    "Display helpful information for debbugging in case of errors/exceptions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/h-ohsaki/perlcompat",
+    url="https://github.com/h-ohsaki/tbdump",
     packages=setuptools.find_packages(),
+    install_requires=['ansiterm'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
